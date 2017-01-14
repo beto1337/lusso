72106f34a1cb22d4f3670b05dc1658e42fd251e9
jesus <jesusleira2011@gmail.com>
2017-01-13 20:39:29 -0500

    1

--- goRegApp.php
+++ goRegApp.php
@@ -17,67 +17,75 @@ $pass   =Encrypt($_POST['passr']);
 $telefono  =$_POST['telefonor'];
 $email  =$_POST['correor'];
 $cod1   = generarCodigo(6);
-
-$sql3   =  $db -> query("SELECT codigo_user FROM users WHERE codigo_user='$cod1' LIMIT 1;");
-$cod2   = $db-> recorrer($sql3)[0];
-$db-> liberar($sql3);
-$sql    = $db -> query("SELECT email, telefono FROM users WHERE (telefono='$telefono' OR email='$email')  LIMIT 1;");
+$verificar= strlen($telefono);
 $response=array();
 $response['success']=false;
-if ($db->rows($sql) == 0) {
-     while ($cod1 == $cod2) {
-        $cod1 = generarCodigo(6);
-        $sql3 =  $db -> query("SELECT codigo_user FROM users WHERE codigo_user='$cod1' LIMIT 1;");
-        $cod2 = $db-> recorrer($sql3)[0];
-        $db-> liberar($sql3);
-      }
-      $keyreg = md5(time());
-      $link= APP_URL.'?view=activar&key='. $keyreg;
 
-$mail = new PHPMailer;
-$mail->CharSet = "UTF-8";
-$mail->Encoding = "quoted-printable";
-$mail->isSMTP();                                    // Set mailer to use SMTP
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Host = PHPMAILER_HOST;  // Specify main and backup SMTP servers
-$mail->Port = PHPMAILER_PORT;                                    // TCP port to connect to
-$mail->Username = PHPMAILER_USER;                 // SMTP username
-$mail->Password = PHPMAILER_PASS;                           // SMTP password
+if ($verificar==10) {
 
-$mail->setFrom(PHPMAILER_USER, APP_TITTLE);         //quien manda el correo
+  $sql3   =  $db -> query("SELECT codigo_user FROM users WHERE codigo_user='$cod1' LIMIT 1;");
+  $cod2   = $db-> recorrer($sql3)[0];
+  $db-> liberar($sql3);
+  $sql    = $db -> query("SELECT email, telefono FROM users WHERE (telefono='$telefono' OR email='$email')  LIMIT 1;");
+  if ($db->rows($sql) == 0) {
+       while ($cod1 == $cod2) {
+          $cod1 = generarCodigo(6);
+          $sql3 =  $db -> query("SELECT codigo_user FROM users WHERE codigo_user='$cod1' LIMIT 1;");
+          $cod2 = $db-> recorrer($sql3)[0];
+          $db-> liberar($sql3);
+        }
+        $keyreg = md5(time());
+        $link= APP_URL.'?view=activar&key='. $keyreg;
 
-$mail->addAddress($email, $nombre);     // quien recibe
+  $mail = new PHPMailer;
+  $mail->CharSet = "UTF-8";
+  $mail->Encoding = "quoted-printable";
+  $mail->isSMTP();                                    // Set mailer to use SMTP
+  $mail->SMTPAuth = true;                               // Enable SMTP authentication
+  $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+  $mail->Host = PHPMAILER_HOST;  // Specify main and backup SMTP servers
+  $mail->Port = PHPMAILER_PORT;                                    // TCP port to connect to
+  $mail->Username = PHPMAILER_USER;                 // SMTP username
+  $mail->Password = PHPMAILER_PASS;                           // SMTP password
 
-$mail->isHTML(true);                                  // Set email format to HTML
+  $mail->setFrom(PHPMAILER_USER, APP_TITTLE);         //quien manda el correo
 
-$mail->Subject = 'Activacion de tu cuenta ';
-$mail->Body    =  EmailTemplate($nombre,$link);
-$mail->AltBody = 'Hola '.$nombre.' para activar tu cuenta accede al siguiente enlace '. $link;
+  $mail->addAddress($email, $nombre);     // quien recibe
 
-if(!$mail->send()) {
-    $response['message']= $mail->ErrorInfo;
+  $mail->isHTML(true);                                  // Set email format to HTML
 
-} else {
+  $mail->Subject = 'Activacion de tu cuenta ';
+  $mail->Body    =  EmailTemplate($nombre,$link);
+  $mail->AltBody = 'Hola '.$nombre.' para activar tu cuenta accede al siguiente enlace '. $link;
 
-$sql_2=$db->query("INSERT INTO users (nombre,apellido,pass,email,telefono,codigo_user,keyreg) VALUES ('$nombre','$apellido','$pass','$email','$telefono','$cod1','$key');");
-  $db-> liberar($sql_2);
-$response['success']=true;
-$response['message']='ha sido registrado satisfactoriamente, ahora solo queda revisar su correo y activar su cuenta';
-}
+  if(!$mail->send()) {
+      $response['message']= $mail->ErrorInfo;
 
-}else {
+  } else {
 
-  $dato = $db->recorrer($sql);
-  $emailc=$dato[0];
-  $telefonoc = $dato[1];
-  if ((strtolower($email) == strtolower($emailc)) ) {
-    $response['message']="El email ingresado se encuentra registrado".$emailc . $telefonoc;
-  }else {
-    $response['message']=$emailc.'este es el correo'.$telefonoc.'otro'.$telefonoc2  ;
+  $sql_2=$db->query("INSERT INTO users (nombre,apellido,pass,email,telefono,codigo_user,keyreg) VALUES ('$nombre','$apellido','$pass','$email','$telefono','$cod1','$key');");
+    $db-> liberar($sql_2);
+  $response['success']=true;
+  $response['message']='ha sido registrado satisfactoriamente, ahora solo queda revisar su correo y activar su cuenta';
   }
 
+  }else {
+
+    $dato = $db->recorrer($sql);
+    $emailc=$dato[0];
+    $telefonoc = $dato[1];
+    if ((strtolower($email) == strtolower($emailc)) ) {
+      $response['message']="El email ingresado se encuentra registrado en el sistema.";
+    }else {
+      $response['message']="el numero ingresado se encuentra registrado en elsistema."  ;
+    }
+
+  }
+}else {
+
+$response['message']="el numero debe tener 10 caracteres" ;
 }
+
 
 $db-> liberar($sql);
 $db-> close();
